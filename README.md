f793038892b83ae0afc103bfb4585b7b3b6229c0
EL HAITI IKRAME <ikrameelhaiti@gmail.com>
2025-01-26 20:44:12 +0100

    Exercice 1 : 1.2 Implémentation du debounce sur la recherche

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 2. J ai passe la fonction setSearchTerm comme prop a ProductSearch pour à chaque fois que l user tape le champ de recherche met à ajoour seartTerm dans App
 3. dans productList j'ai utilisé searchTerm pour filtrer les produits avant de les affichers 
 
+![alt text](image.png)
+
+# 1.2 Implémenter le debounce sur la recherche
+
+1. j'ai créé un fichier nommé hooks/useDebounce.js ou j'ai définit un hook qui ajoute un debounce a une valeur ( attend une période avant de mettre a jour la valeur)
+2. Ensuite, je me suis occupée du composant ProductSearch pour qu'il utilise le hook useDebounce.j'ai importé le hook , j'ai ajouté un état local pour capturer les saisies immédiates de l'utilisateur et j'ai appliqué useDebounce à localSearch pour obtenir une version retardée appelée debouncedSearchTerm.Enfin, avec un useEffect, j'ai mis à jour l'état global searchTerm après que le délai de debounce ait pris fin.
--- src/components/ProductSearch.js
+++ src/components/ProductSearch.js
@@ -1,18 +1,25 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { ThemeContext } from '../App';
+import useDebounce from '../hooks/useDebounce';
 
 const ProductSearch = ({searchTerm , setSearchTerm}) => {
+  const [localSearch , setLocalSearch] = useState(searchTerm);
   const { isDarkTheme } = useContext(ThemeContext);
   // TODO: Exercice 2.1 - Utiliser le LanguageContext
   
   // TODO: Exercice 1.2 - Utiliser le hook useDebounce
-  
+  const debouncedSearchTerm = useDebounce(localSearch , 500);
+
+  useEffect(() => {
+    setSearchTerm(debouncedSearchTerm)
+  }, [debouncedSearchTerm, searchTerm])
+
   return (
     <div className="mb-4">
       <input
         type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        value={localSearch}
+        onChange={(e) => setLocalSearch(e.target.value)}
         placeholder="Rechercher un produit..."
         className={`form-control ${isDarkTheme ? 'bg-dark text-light' : ''}`}
       />
